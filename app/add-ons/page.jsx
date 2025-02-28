eedaaf11a40c5672f604e3cbbbae0c21bcd64c7d
Hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-02-28 17:16:15 +0500

    Design Addons Form

--- app/add-ons/page.jsx
+++ app/add-ons/page.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/app/components/add-ons/HeroSection"
 import Addons from "@/app/components/add-ons/AddOns"
+import Form from "@/app/components/add-ons/Form"
 export default function AddOns() {
   return (
     <main>
         <HeroSection/>
         <Addons/>
+        <Form/>
     </main>
   );
 }
---
+++ app/components/add-ons/Form.jsx
@@ -0,0 +1,60 @@
+import { Button, Label, Textarea, TextInput } from "flowbite-react";
+import React from "react";
+
+const Form = () => {
+  return (
+    <section className="my-14 py-10 bg-white">
+      <div className="mx-4 md:mx-16">
+        <div className="text-center">
+          <h3 className="text-2xl font-semibold">
+            Not Mentioned! What You Are Looking For? Contact Us Now
+          </h3>
+          <p className="mt-1">
+            Add-ons are additional services that you can purchase to enhance
+            your website
+          </p>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="w-full md:w-[80%]">
+            <div className="flex flex-col gap-5 mt-5">
+              <div>
+                <Label htmlFor="name">Name</Label>
+                <TextInput
+                  id="name"
+                  type="text"
+                  placeholder="John Doe"
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <Label htmlFor="name">Email</Label>
+                <TextInput
+                  id="email"
+                  type="email"
+                  placeholder="john@gmail.com"
+                  className="w-full"
+                />
+              </div>
+              <div>
+                <Label htmlFor="suggestion">Addons Suggestion</Label>
+                <Textarea
+                  id="suggestion"
+                  placeholder="Write your suggestion here"
+                  className="w-full"
+                  rows={5}
+                />
+              </div>
+              <div className="flex items-center justify-center">
+                <Button type="submit" color="blue" size="md">
+                  Submit
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Form;
