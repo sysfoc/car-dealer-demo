3889e318eb48b58aef5e3e71d3c87c37c390da95
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-04-24 16:13:14 +0500

    Minor Fixes

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,14 +1,8 @@
-import OverviewSection from "@/app/(admin)/components/dashboard/Overview";
-import ChartsSection from "@/app/(admin)/components/dashboard/Charts";
 import OrdersSection from "@/app/(admin)/components/dashboard/OrdersTable";
-import GeoChartSection from "@/app/(admin)/components/dashboard/Statistics";
 export default function Dashboard() {
   return (
     <main>
-      {/* <OverviewSection />
-      <ChartsSection /> */}
       <OrdersSection />
-      {/* <GeoChartSection /> */}
     </main>
   );
 }
