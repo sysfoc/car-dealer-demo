f292b52a2d45917a8b33f4808fe1deec33508247
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-07-03 10:58:59 +0500

    Contact Form Api Upgraded

--- app/api/user/contact/route.js
+++ app/api/user/contact/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/app/api/utils/db";
 import Contact from "@/app/model/contact.model";
-
+import { sendEmail } from "@/app/api/utils/send-email";
+import { config } from "@/app/api/utils/env-config";
 export async function POST(req) {
   await connectToDatabase();
   const { name, email, message } = await req.json();
@@ -13,6 +14,11 @@ export async function POST(req) {
   }
   try {
     const contact = await Contact.create({ name, email, message });
+    await sendEmail({
+      to: config.emailReceiver,
+      subject: "Contact Form Submission - Automotiv Web Solutions",
+      text: `A User has submitted a contact form:\nName: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    })
     if (!contact) {
       return NextResponse.json(
         { message: "Something went wrong" },
