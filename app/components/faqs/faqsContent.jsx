32d783cd18e22bfa665e0f76e22eb4dd313dc0e4
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-07-10 11:51:18 +0500

    Add Faqs In the Site

---
+++ app/components/faqs/faqsContent.jsx
@@ -0,0 +1,136 @@
+"use client";
+
+import React, { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { faqs } from "@/lib/faqs/faq";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+const faqsContent = () => {
+  const [openCategoryIndex, setOpenCategoryIndex] = useState(null);
+  const [openQuestions, setOpenQuestions] = useState({});
+
+  const toggleCategory = (index) => {
+    setOpenCategoryIndex((prev) => (prev === index ? null : index));
+  };
+
+  const toggleQuestion = (catIndex, qIndex) => {
+    const key = `${catIndex}-${qIndex}`;
+    setOpenQuestions((prev) => ({
+      ...prev,
+      [key]: !prev[key],
+    }));
+  };
+  return (
+    <section className='dark:bg-gray-900 my-10'>
+      <div>
+        <h1 className='text-center text-3xl font-bold text-gray-800 dark:text-white'>
+          Frequently Asked Questions (FAQ)
+        </h1>
+      </div>
+
+      <div className='mt-10 mx-auto w-full md:w-[90%] flex flex-col gap-4 px-4'>
+        {faqs.map((faq, catIndex) => {
+          const isCategoryOpen = openCategoryIndex === catIndex;
+          return (
+            <div
+              key={catIndex}
+              className='border border-gray-200 dark:border-gray-700 rounded-md overflow-hidden'
+            >
+              <div
+                className='flex items-center justify-between gap-4 bg-white px-6 py-5 dark:bg-gray-800 cursor-pointer'
+                onClick={() => toggleCategory(catIndex)}
+              >
+                <h2 className='text-[22px] font-semibold text-gray-900 dark:text-white'>
+                  {faq.categoryHeader}
+                </h2>
+                <div className='flex flex-row gap-3'>
+                  <div>
+                    <span className='bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded'>
+                      {faq.category}
+                    </span>
+                  </div>
+                  <div>
+                    <IoIosArrowDown
+                      fontSize={24}
+                      className={`transition-transform duration-300 ${
+                        isCategoryOpen ? "rotate-180" : ""
+                      }`}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div
+                className={`overflow-hidden transition-all duration-500 ease-in-out px-6 border-t bg-white/80 dark:bg-gray-800 ${
+                  isCategoryOpen
+                    ? "opacity-100 max-h-[1000px] py-5"
+                    : "opacity-0 max-h-0 py-0"
+                }`}
+              >
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4'>
+                  {faq.questions.map((question, qIndex) => {
+                    const key = `${catIndex}-${qIndex}`;
+                    const isQuestionOpen = openQuestions[key];
+
+                    return (
+                      <div key={key} className='transition-all'>
+                        <div
+                          className='mb-2 flex justify-between gap-3 cursor-pointer'
+                          onClick={() => toggleQuestion(catIndex, qIndex)}
+                        >
+                          <h3 className='text-lg font-semibold text-gray-900 dark:text-white'>
+                            {question.question}
+                          </h3>
+                          <div>
+                            {isQuestionOpen ? (
+                              <FaMinus
+                                fontSize={16}
+                                className='mt-1 text-gray-700 dark:text-gray-300'
+                              />
+                            ) : (
+                              <FaPlus
+                                fontSize={16}
+                                className='mt-1 text-gray-700 dark:text-gray-300'
+                              />
+                            )}
+                          </div>
+                        </div>
+                        <div
+                          className={`transition-all duration-300 ease-in-out ${
+                            isQuestionOpen
+                              ? "opacity-100 max-h-[500px] mt-1"
+                              : "opacity-0 max-h-0 overflow-hidden"
+                          }`}
+                        >
+                          {question.answer.split("\n\n").map((block, idx) => {
+                            const isBullet =
+                              idx > 0 &&
+                              (block.includes(":") || block.length > 30);
+                            return isBullet ? (
+                              <ul key={idx} className='list-inside'>
+                                <li className='ml-5 list-disc text-gray-600 dark:text-gray-400'>
+                                  {block}
+                                </li>
+                              </ul>
+                            ) : (
+                              <p
+                                key={idx}
+                                className='text-gray-600 dark:text-gray-400 mb-2'
+                              >
+                                {block}
+                              </p>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default faqsContent;
