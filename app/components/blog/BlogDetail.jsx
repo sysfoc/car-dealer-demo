475af5e216a7d37c5cb00e0e71aea68b61dfd9ad
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-07-18 08:39:26 +0500

    Fix Blogs Meta Data

--- app/components/blog/BlogDetail.jsx
+++ app/components/blog/BlogDetail.jsx
@@ -8,14 +8,12 @@ import {
   TextInput,
 } from "flowbite-react";
 import Head from "next/head";
-import Image from "next/image";
 import { useParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { IoMdAlarm } from "react-icons/io";
 
-const BlogDetail = () => {
+const BlogDetail = ({ blog }) => {
   const params = useParams();
-  const [blog, setBlog] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
@@ -47,10 +45,6 @@ const BlogDetail = () => {
       const data = await res.json();
       setLoading(false);
       if (res.ok) {
-        setBlog({
-          ...blog,
-          comments: [...blog.comments, data.comment],
-        });
         setFormData({
           slug: params.slug,
           fname: "",
@@ -71,127 +65,117 @@ const BlogDetail = () => {
     }
   };
   return (
-    <>
-      <Head>
-        <title>{`${blog.title} - SYSFOC Car Dealer`}</title>
-        <meta name='description' content={blog.metaDescription} />
-        <meta
-          property='og:title'
-          content={`${blog.title} - SYSFOC Car Dealer`}
-        />
-        <meta property='og:description' content={blog.metaDescription} />
-      </Head>
-      <section className='mx-4 my-5 sm:mx-16'>
-        <div className='grid grid-cols-1 items-center gap-x-10 gap-y-5 py-5 md:grid-cols-2'>
-          <div className='overflow-hidden rounded-lg'>
-            <Image
-              src={blog.image || "/banner.webp"}
-              alt={`${blog.title}-img`}
-              width={500}
-              height={300}
-              priority
-              className='size-full'
-            />
-          </div>
-          <div>
-            <div className='flex flex-row items-center gap-3'>
-              <Button className='bg-red-600 hover:!bg-red-700' size='sm'>
-                Car Blog
-              </Button>
-              <div className='flex items-center gap-2'>
-                <IoMdAlarm fontSize={18} />
-                <span>
-                  {new Date(blog.createdAt).toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
-                </span>
-              </div>
+    <section className='mx-4 my-5 sm:mx-16'>
+      <div className='grid grid-cols-1 items-center gap-x-10 gap-y-5 py-5 md:grid-cols-2'>
+        <div className='overflow-hidden rounded-lg'>
+          <img
+            src={blog.image || "/banner.webp"}
+            alt={`${blog.title}-img`}
+            width={500}
+            height={300}
+            className='size-full'
+          />
+        </div>
+        <div>
+          <div className='flex flex-row items-center gap-3'>
+            <Button className='bg-red-600 hover:!bg-red-700' size='sm'>
+              Car Blog
+            </Button>
+            <div className='flex items-center gap-2'>
+              <IoMdAlarm fontSize={18} />
+              <span>
+                {new Date(blog.createdAt).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
             </div>
-            <h1 className='mt-3 text-2xl font-bold sm:mt-5 sm:text-4xl'>
-              {blog.title}
-            </h1>
-            <div className='mt-5 flex items-center gap-10'>
-              <div className='flex items-center gap-3'>
-                <Avatar size={"sm"} rounded />
-                <span>{blog.blogWriter}</span>
-              </div>
+          </div>
+          <h1 className='mt-3 text-2xl font-bold sm:mt-5 sm:text-4xl'>
+            {blog.title}
+          </h1>
+          <div className='mt-5 flex items-center gap-10'>
+            <div className='flex items-center gap-3'>
+              <Avatar size={"sm"} rounded />
+              <span>{blog.blogWriter}</span>
             </div>
           </div>
         </div>
-        <div className='mt-5 w-full md:w-3/4'>
-          <div
-            className='dynamic-content prose dark:prose-invert flex flex-col gap-y-3'
-            dangerouslySetInnerHTML={{ __html: blog.content }}
-          />
-          <div className='bg-white p-5 mt-8'>
-            <h2 className='text-2xl font-bold'>Comments</h2>
-            <div>
-              <form onSubmit={handleFormData}>
-                {error && (
-                  <Alert color='failure'>
-                    <span>
-                      <span className='font-medium'>Error! </span>
-                      {errorMessage}
-                    </span>
-                  </Alert>
-                )}
-                <div className='mt-3 grid grid-cols-1 gap-3 sm:grid-cols-2'>
-                  <div className='flex flex-col'>
-                    <Label htmlFor='fname'>First Name:</Label>
-                    <TextInput
-                      type='text'
-                      id='fname'
-                      placeholder='First Name'
-                      required
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className='flex flex-col'>
-                    <Label htmlFor='lname'>Last Name:</Label>
-                    <TextInput
-                      type='text'
-                      id='lname'
-                      placeholder='Last Name'
-                      required
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className='col-span-2 flex flex-col'>
-                    <Label htmlFor='email'>Email:</Label>
-                    <TextInput
-                      type='email'
-                      id='email'
-                      placeholder='Email Address'
-                      required
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <div className='col-span-2 flex flex-col'>
-                    <Label htmlFor='comment'>Add Comment:</Label>
-                    <Textarea
-                      rows={10}
-                      id='comment'
-                      required
-                      onChange={handleChange}
-                    />
-                  </div>
+      </div>
+      <div className='mt-5 w-full md:w-3/4'>
+        <div
+          className='dynamic-content prose dark:prose-invert flex flex-col gap-y-3'
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        />
+        <div className='bg-white p-5 mt-8'>
+          <h2 className='text-2xl font-bold'>Comments</h2>
+          <div>
+            <form onSubmit={handleFormData}>
+              {error && (
+                <Alert color='failure'>
+                  <span>
+                    <span className='font-medium'>Error! </span>
+                    {errorMessage}
+                  </span>
+                </Alert>
+              )}
+              <div className='mt-3 grid grid-cols-1 gap-3 sm:grid-cols-2'>
+                <div className='flex flex-col'>
+                  <Label htmlFor='fname'>First Name:</Label>
+                  <TextInput
+                    type='text'
+                    id='fname'
+                    placeholder='First Name'
+                    required
+                    onChange={handleChange}
+                  />
                 </div>
-                <div>
-                  <Button
-                    type='submit'
-                    disabled={loading}
-                    size={"md"}
-                    className=' mt-5 bg-red-600 hover:!bg-red-700'
-                  >
-                    Add Comment
-                  </Button>
+                <div className='flex flex-col'>
+                  <Label htmlFor='lname'>Last Name:</Label>
+                  <TextInput
+                    type='text'
+                    id='lname'
+                    placeholder='Last Name'
+                    required
+                    onChange={handleChange}
+                  />
                 </div>
-              </form>
-            </div>
-            <div>
-              {!loading && blog?.comments?.map((comment, index) => (
+                <div className='col-span-2 flex flex-col'>
+                  <Label htmlFor='email'>Email:</Label>
+                  <TextInput
+                    type='email'
+                    id='email'
+                    placeholder='Email Address'
+                    required
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='col-span-2 flex flex-col'>
+                  <Label htmlFor='comment'>Add Comment:</Label>
+                  <Textarea
+                    rows={10}
+                    id='comment'
+                    required
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div>
+                <Button
+                  type='submit'
+                  disabled={loading}
+                  size={"md"}
+                  className=' mt-5 bg-red-600 hover:!bg-red-700'
+                >
+                  Add Comment
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div>
+            {!loading &&
+              blog?.comments?.map((comment, index) => (
                 <div
                   key={index}
                   className='mt-5 border border-gray-300 p-5 dark:border-gray-600'
@@ -208,11 +192,10 @@ const BlogDetail = () => {
                   <p className='mt-3'>{comment?.comment}</p>
                 </div>
               ))}
-            </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
